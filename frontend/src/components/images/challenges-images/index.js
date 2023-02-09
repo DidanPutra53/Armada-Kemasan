376aa315166de2378a1f68d20da8ce76f8a06f7b
DidanPutra53 <didanputra75@gmail.com>
2023-02-09 23:44:17 +0700

    initial commit

--- frontend/src/components/images/challenges-images/index.js
+++ frontend/src/components/images/challenges-images/index.js
@@ -1,5 +1,5 @@
-import challenges from './many-kinds-garbage-were-collected-dark-floor.jpg';
-import challenges1 from './plastic-containers-cleaned-before-recycling.jpg';
-import challenges2 from './trash-bags.jpg'
+import challenges from './many-kinds-garbage-were-collected-dark-floor.webp';
+import challenges1 from './plastic-containers-cleaned-before-recycling.webp';
+import challenges2 from './trash-bags.webp'
 
 export { challenges, challenges1, challenges2 }
