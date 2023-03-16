c3f593407af7f8b6819ed8596eb9a99f2d9ca5bf
DidanPutra53 <didanputra75@gmail.com>
2023-03-16 17:53:46 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { useEffect } from 'react';
 import './App.scss';
 import './AppPhone.scss';
